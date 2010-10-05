21afc00a436cb09957eae853b8516b787293557b
Olex <tholex@gmail.com>
2010-10-05 14:29:06 -0400

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hooverdam::Application.config.secret_token = 'e4e5733ead9d7f56b543f61d903ae4c7309db9b6dbb269f7044ababf0852d7acecc9c6a322c0aea1b1b95aa5eef19fe9ee2df5e9046b230ace493589e90dddf2'
