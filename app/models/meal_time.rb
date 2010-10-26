9ba0b9224fe8a1ad007a81c0406cf4953e83a131
Olex <tholex@gmail.com>
2010-10-26 07:20:29 -0400

    PostgreSQL interoperability

--- app/models/meal_time.rb
+++ app/models/meal_time.rb
@@ -5,42 +5,42 @@ class MealTime < ActiveRecord::Base
   has_many :menu_items
   include Constants
   
-  scope :todays, lambda { where(['DATE(start) = ?', Date.today]) }
-  scope :breakfast, lambda { where(['TIME(start) = ?', BREAKFAST_START_TIME ])}
-  scope :brunch, lambda { where(['TIME(start) = ?', BRUNCH_START_TIME ])}
-  scope :dinner, lambda { where(['TIME(start) = ?', DINNER_START_TIME ])}
-  scope :lunch, lambda { where(['TIME(start) = ?', LUNCH_START_TIME ])}
-  scope :after_hours, lambda { where(['TIME(start) = ?', AFTER_HOURS_START_TIME ])}
-  scope :soup_salad, lambda { where(['TIME(start) = ?', SOUP_SALAD_START_TIME ])}
-  scope :salad, lambda { where(['TIME(start) = ?', SALAD_START_TIME ])}
+  scope :todays, lambda { where(['start > ?', Date.today.gmtime + EST_OFFSET]) }
+  scope :breakfast, lambda { where(['start = ?', BREAKFAST_START ])}
+  scope :brunch, lambda { where(['start = ?', BRUNCH_START ])}
+  scope :dinner, lambda { where(['start = ?', DINNER_START ])}
+  scope :lunch, lambda { where(['start = ?', LUNCH_START ])}
+  scope :after_hours, lambda { where(['start = ?', AFTER_HOURS_START ])}
+  scope :soup_salad, lambda { where(['start = ?', SOUP_SALAD_START ])}
+  scope :salad, lambda { where(['start = ?', SALAD_START ])}
   
   def self.lunch!(opts = {})
     options = {:start => LUNCH_START, :end => LUNCH_END, :meal => "Lunch"}.merge(opts)
-    where(opts).todays.lunch.first || create(options)
+    where(opts).lunch.first || create(options)
   end
   def self.brunch!(opts = {})
     options = {:start => BRUNCH_START, :end => BRUNCH_END, :meal => "Brunch"}.merge(opts)
-    where(opts).todays.brunch.first || create(options)
+    where(opts).brunch.first || create(options)
   end
   def self.breakfast!(opts = {})
     options = {:start => BREAKFAST_START, :end => BREAKFAST_END, :meal => "Breakfast"}.merge(opts)
-    where(opts).todays.breakfast.first || create(options)
+    where(opts).breakfast.first || create(options)
   end
   def self.dinner!(opts = {})
     options = {:start => DINNER_START, :end => DINNER_END, :meal => "Dinner"}.merge(opts)
-    where(opts).todays.dinner.first || create(options)
+    where(opts).dinner.first || create(options)
   end
   def self.after_hours!(opts = {})
     options = {:start => AFTER_HOURS_START, :end => AFTER_HOURS_END, :meal => "After Hours"}.merge(opts)
-    where(opts).todays.after_hours.first || create(options)
+    where(opts).after_hours.first || create(options)
   end
   def self.soup_salad!(opts = {})
     options = {:start => SOUP_SALAD_START, :end => SOUP_SALAD_END, :meal => "Soup And Salad"}.merge(opts)
-    where(opts).todays.soup_salad.first || create(options)
+    where(opts).soup_salad.first || create(options)
   end
   def self.salad!(opts = {})
     options = {:start => SALAD_START, :end => SALAD_END, :meal => "Salad"}.merge(opts)
-    where(opts).todays.salad.first || create(options)
+    where(opts).salad.first || create(options)
   end
   
 end
